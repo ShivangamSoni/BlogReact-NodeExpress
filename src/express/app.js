6a6caf43e60c60300b2272aca2f68f75a4cb2d6d
Shivangam Soni <shivangamsoni99@gmail.com>
2022-01-19 18:20:32 +0530

    CORS Config

--- src/express/app.js
+++ src/express/app.js
@@ -3,10 +3,9 @@ const cors = require("cors");
 const app = express();
 const BlogsRouter = require("./Routers/blogs");
 const UserRouter = require("./Routers/user");
-const { CORS_Config } = require("./CONSTANTS");
 
 app.use(express.json());
-app.use(cors(CORS_Config));
+app.use(cors());
 app.use("/api/v1/blogs", BlogsRouter);
 app.use("/api/v1/user", UserRouter);
 
